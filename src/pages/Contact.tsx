3818872603b388916bb80d535be8b3b029fecd7f
Danilo <danilocesardasilva53@gmail.com>
2025-06-08 13:03:24 -0300

    adicionando mapa

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -277,9 +277,16 @@ const Contact = () => {
 
                     <div className="max-w-5xl mx-auto mt-12">
                         <div className="bg-white rounded-lg shadow-md p-4 h-96 flex items-center justify-center">
-                            <p className="text-gray-500">
-                                Mapa será carregado aqui
-                            </p>
+                            <iframe
+                                title="Mapa da Casa do Adalto"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3577.6708876973785!2d-48.8857078!3d-26.2723428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94deafebb8b53bcf%3A0x2d1d33ae8a2276a9!2zQXNzb2NpYcOnw6NvIENhc2EgZG8gQWRhbHRvIEFwb2lvIMOgcyBDcmlhbsOnYXMgYy8gQ8OibmNlcg!5e0!3m2!1spt-BR!2sbr!4v1749397250077!5m2!1spt-BR!2sbr"
+                                width="100%"
+                                height="100%"
+                                style={{ border: 0 }}
+                                allowFullScreen={true}
+                                loading="lazy"
+                                referrerPolicy="no-referrer-when-downgrade"
+                            ></iframe>
                         </div>
                     </div>
                 </div>
